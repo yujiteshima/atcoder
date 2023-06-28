518fbdeab9b6b17dd77d4238cbf899d6f7b30c69
yujiteshima <yuji.teshima@gmail.com>
2023-06-28 21:44:43 +0900

    ac

---
+++ tessoku/a02.py
@@ -0,0 +1,15 @@
+# Linear Search
+N, X = map(int, input().split())
+
+A = list(map(int, input().split()))
+
+found = False
+
+for a in A:
+    if X == a:
+        found = True
+
+if found:
+    print("Yes")
+else:
+    print("No")
