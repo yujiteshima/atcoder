9cd2d6ae256848cabfff6ea99d34aea7f09bf40e
yujiteshima <yuji.teshima@gmail.com>
2023-06-30 01:53:22 +0900

    ac

--- tessoku/a06.py
+++ tessoku/a06.py
@@ -1,5 +1,5 @@
 N, Q = map(int, input().split())
-A = list(map(int, input(split())))
+A = list(map(int, input().split()))
 # Caluculate the cumlative sum in advance
 accum = [0] * N
 accum[0] = A[0]
@@ -9,5 +9,5 @@ for _ in range(Q):
     L, R = map(int, input().split())
     L -= 1
     R -= 1
-    ans = accum[R+1] - accum[L]
+    ans = accum[R] - (accum[L-1] if L > 0 else 0)
     print(ans)
